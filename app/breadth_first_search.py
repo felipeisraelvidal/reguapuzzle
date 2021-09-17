f031862257ccdf989913d0897768a80dddf41430
Felipe Vidal <felipeisra.vidal@gmail.com>
2021-09-17 00:40:13 -0300

    Uniform cost search done

--- app/breadth_first_search.py
+++ app/breadth_first_search.py
@@ -16,37 +16,60 @@ class BreadthFirstSearch(Algorithm):
     _game_state = GameState.PLAYING
     __final_state = None
 
-    _qtd_moves = 0
+    __qtd_visited_nodes = 0
+    __qtd_expanded_nodes = 0
 
-    def __generate_next_state(self, state, callback):
+    __tree_height = 0
+
+    def generate_next_states(self, state):
         current_distance = 1
 
-        if state.value in self.__closed_states_values.values():
+        self.__tree_height = max([state.height, self.__tree_height])
+
+        if state.value in self.__closed_states_values:
             return
 
         for _ in range(self.qtd_blocks):
             empty_index = state.value.find('-')
 
             if current_distance <= self.qtd_blocks:
-                # Mudar para a esquerda
+                # Move para a esquerda
                 if empty_index - current_distance >= 0:
                     new_state_value = state.value
                     new_state_value = replacer(new_state_value, new_state_value[empty_index - current_distance], empty_index)
                     new_state_value = replacer(new_state_value, '-', empty_index - current_distance)
 
-                    new_state = State.get_state_from_value(self.qtd_blocks, new_state_value, state.id, height=state.height + 1)
-                    callback(new_state)
+                    if new_state_value not in self.__closed_states_values and new_state_value not in self.__open_dict:
+                        new_state = State(new_state_value, state.id, current_distance, height=state.height + 1)
+                        self.__qtd_expanded_nodes = self.__qtd_expanded_nodes + 1
 
-                if empty_index + current_distance <= len(state.value) - 1: # Mudar para a direita
+                        if self.__is_solution(new_state):
+                            return new_state
+
+                        self.__open_queue.append(new_state)
+                        self.__open_dict[new_state_value] = new_state
+
+                # Move para a direita
+                if empty_index + current_distance <= len(state.value) - 1:
                     new_state_value = state.value
                     new_state_value = replacer(new_state_value, new_state_value[empty_index + current_distance], empty_index)
                     new_state_value = replacer(new_state_value, '-', empty_index + current_distance)
 
-                    new_state = State.get_state_from_value(self.qtd_blocks, new_state_value, state.id, height=state.height + 1)
-                    callback(new_state)
+                    if new_state_value not in self.__closed_states_values and new_state_value not in self.__open_dict:
+                        new_state = State(new_state_value, state.id, current_distance, height=state.height + 1)
+                        self.__qtd_expanded_nodes = self.__qtd_expanded_nodes + 1
+
+                        if self.__is_solution(new_state):
+                            return new_state
+
+                        self.__open_queue.append(new_state)
+                        self.__open_dict[new_state_value] = new_state
 
                 current_distance = current_distance + 1
 
+    def __is_solution(self, state):
+        return state.value in self.final_states
+    
     def __push_into_open_queue(self, state):
         self.__open_queue.append(state)
         self.__open_dict[state.value] = True
@@ -91,35 +114,19 @@ class BreadthFirstSearch(Algorithm):
         start_time = time.time()
 
         while self._game_state == GameState.PLAYING:
-            if len(self.__open_queue) < 1: # the open queue is empty
+            if len(self.__open_queue) < 1:
                 self._game_state = GameState.FAIL
             else:
                 n = self.__pop_from_open_queue(0)
+                self.__qtd_visited_nodes = self.__qtd_visited_nodes + 1
 
-                if self._Algorithm__is_solution(n):
-                    self.__final_state = n
+                x = self.generate_next_states(n)
+                if x != None:
+                    self.__final_state = x
                     self._game_state = GameState.SUCCESS
-                else:
-                    if n.value in self.__closed_states_values:
-                        self.__final_state = n
-                        self._game_state = GameState.SUCCESS
-                    else:
-                        def validate_new_state(state):
-                            self._qtd_moves = self._qtd_moves + 1
-                            # +1 nó gerado
 
-                            if self._Algorithm__is_solution(state):
-                                self.__final_state = state
-                                self._game_state = GameState.SUCCESS
-                            else:
-                                if state.value not in self.__closed_states_values and state.value not in self.__open_dict:
-                                    self.__push_into_open_queue(state)
-                        
-                        self.__generate_next_state(n, validate_new_state)
-                        # +1 nó visitado
-                        
-                        self.__closed_states_values[n.value] = n
-                        self.__closed_states_ids[n.id] = n
+                self.__closed_states_values[n.value] = n
+                self.__closed_states_ids[n.id] = n
         
         stop_time = time.time()
         
@@ -136,11 +143,13 @@ class BreadthFirstSearch(Algorithm):
         else:
             print('Path:\n\t-')
             print(f'Profundidade: -1')
+
+        average_ramification_factor = self.__qtd_expanded_nodes / (self.__tree_height + 1)
+        average_ramification_factor = round(average_ramification_factor, 2)
         
-        print(f'Custo da Solução:')
-        print(f'Total de Nós Expandidos:')
-        print(f'Total de Nós Visitados:')
-        print(f'Valor Médio do Fator de Ramificação da Árvore:')
+        print(f'Total de Nós Expandidos: {self.__qtd_expanded_nodes}')
+        print(f'Total de Nós Visitados: {self.__qtd_visited_nodes}')
+        print(f'Valor Médio do Fator de Ramificação da Árvore: {average_ramification_factor}')
         print(f'Execution time: {stop_time - start_time}s')
         
         print(f'{color}Finished Game: ', self._game_state, bcolors.ENDC)
